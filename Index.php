794df9dff3f82ee9090bccef98c9d54bb4476464
m-p-schiller <56848369+m-p-schiller@users.noreply.github.com>
2019-11-06 09:43:53 -0500

    Add files via upload

--- Index.php
+++ Index.php
@@ -1,20 +1,69 @@
+<!DOCTYPE html>
 <?php
 include 'db_connection.php';
 $conn = OpenCon();
 ?>
 <html>
 <head>
+
 	<link rel="stylesheet" href="MainStyle.css">
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+    $('.search-box input[type="text"]').on("keyup input", function(){
+        /* Get input value on change */
+        var inputVal = $(this).val();
+        var resultDropdown = $(this).siblings(".result");
+        if(inputVal.length){
+            $.get("livesearch.php", {term: inputVal}).done(function(data){
+                // Display the returned data in browser
+                resultDropdown.html(data);
+            });
+        } else{
+            resultDropdown.empty();
+        }
+    });
+    
+    // Set search input value on click of result item
+    $(document).on("click", ".result p", function(){
+        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+        $(this).parent(".result").empty();
+    });
+});
+
+$(document).on("keypress", "input", function(e){
+    if(e.which == 13){
+        var inputVal = $(this).val();
+		inputVal = inputVal.replace(/\s/g, '_');
+		inputVal = inputVal.replace(/'/g, '_');		
+		inputVal = inputVal.replace(/[^\w\s]/g, "");
+        window.location.href = "http://24.57.198.146/library/BookPage.php?DTitle=" + inputVal;
+    }
+});
+</script>
 </head>
-<body>
+<header>
+<div class="container">
+      <div class="header-image"><img src="logo.png" alt=""></div>
+			<div class="intro-heading "><h1><span>Welcome to</span> Unicorn BookStore</h1></div>
+		</div>
+</header>
 
 <div class="topMenu">
-	<a href="Index.php">Liber</a>
+	<a href="Index.php">Home</a>
 	<a href="Genre.php">Genre</a>
 	<a href="About.html">About</a>
-	<a href="#">My Account</a>
+	<a href="Register.php">My Account</a>
 	<a href="#">My Books</a>
+	<div  class="search-box">
+		<input type="text" autocomplete="off" placeholder="Search title..." />
+		<div class="result"></div>
+	</div>
+	
 </div>
+
+
+<body>	
 
 <h1>Recently Added</h1>
 
@@ -26,8 +75,9 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row	
     while($row = $result->fetch_assoc()) {
+		$getLink = preg_replace('/[^a-z]+/i', '_', $row["Title"]); 
 		echo '<div class="book">';
-		echo '<img src="bookArt.png">';
+		echo '<a href="BookPage.php?DTitle=' .$getLink. '"><img src="/Library/BookArt/' . $row["DownloadTitle"] . '.jpg"></a>';
 		echo '<div class="desc"><i>' . $row["Title"]. '</i> </br>' . $row["Author"] .'</div>';
 		echo '</div>';
     }
@@ -36,9 +86,6 @@ if ($result->num_rows > 0) {
 }
 ?>
 
-<div class="footer">
-	
-</div>
 
 </body>
 </html>
